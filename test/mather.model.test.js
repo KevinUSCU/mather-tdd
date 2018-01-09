8b866e58c62d527cde274b3ec86c7b41515b6527
Kevin Springer <KevinUSCU@outlook.com>
2018-01-09 12:08:57 -0800

    Test add for multiple arguments.

--- test/mather.model.test.js
+++ test/mather.model.test.js
@@ -9,6 +9,11 @@ describe('Mather Model', () => {
     test('It should add two numbers', () => {
       expect(model.add(1, 2)).toBe(3)
     })
+    test('It should add any number of numbers', () => {
+      expect(model.add(1)).toBe(1)
+      expect(model.add(1, 2, 10)).toBe(13)
+      expect(model.add(3, 7, -5, 19, 25, -12)).toBe(37)
+    })
   })
 
 })
