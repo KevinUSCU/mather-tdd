3bb8ac0c180426c0c484b56cf54f452b7b407b76
Kevin Springer <KevinUSCU@outlook.com>
2018-01-10 13:26:58 -0800

    Remove temporary route.

--- routes/mather.routes.js
+++ routes/mather.routes.js
@@ -2,8 +2,6 @@ const express = require('express')
 const router = express.Router()
 const MatherControllers = require('../controllers/mather.controllers')
 
-router.get('/addem', MatherControllers.getAddEmUp)
-
 router.post('/addem', MatherControllers.addEmUp)
 
 module.exports = router
