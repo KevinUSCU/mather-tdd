8efccf6b35401e48d2ced4f4d04b098183a62fce
Kevin Springer <KevinUSCU@outlook.com>
2018-01-10 16:23:54 -0800

    Add error message for bad input on addEmUp.

--- controllers/mather.controllers.js
+++ controllers/mather.controllers.js
@@ -4,6 +4,7 @@ class MatherControllers {
 
   static addEmUp(req, res, next) {
     const { numbers } = req.body
+    if (!numbers) return next({ status: 400, message: 'You must provide numbers to add!' })
     const result = MatherModel.add(...numbers)
     return res.status(200).json({ total: result })
   }
